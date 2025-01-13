24e11eeb7af229b85acd24d1ba8733140db4cc30
Delian Delchev <delian.delchev@gmail.com>
2025-01-13 13:59:56 +0000

    working
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- m.sh
+++ m.sh
@@ -1,6 +1,4 @@
-rm -rf ./build
 mkdir -p build
-conan install . --output-folder=build --build=missing
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-cmake --build . --config Release
+cmake ..
+make
