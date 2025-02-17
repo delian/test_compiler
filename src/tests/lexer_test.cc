cb263fb08396f1a90ae88d8e50516585124e142f
Delian Delchev <delian.delchev@gmail.com>
2025-02-17 14:10:58 +0000

    junk
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/tests/lexer_test.cc
+++ src/tests/lexer_test.cc
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <iostream>
+#include <filesystem>
+#include <string>
+
 // Demonstrate some basic assertions.
 TEST(ParsingTest, ParsingFiles)
 {
@@ -7,4 +11,13 @@ TEST(ParsingTest, ParsingFiles)
     EXPECT_STRNE("hello", "hello2");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
-}
+};
+
+TEST(ParsingTest, ParsingFilesDir) {
+    std::string path = "../../lang-tests";
+    std::cout << "Current path is " << std::filesystem::current_path() << std::endl;
+    for (const auto & entry : std::filesystem::directory_iterator(path)) {
+        std::cout << entry.path() << std::endl;
+    }
+    EXPECT_EQ(1, 1);
+};
