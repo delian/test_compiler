615bde7142247a338cf8ac4df033372b9281ac7b
Delian Delchev <delian.delchev@gmail.com>
2025-02-17 13:26:29 +0000

    gtest migrate to conan
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/tests/lexer_test.cc
+++ src/tests/lexer_test.cc
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions)
+TEST(ParsingTest, ParsingFiles)
 {
     // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
+    EXPECT_STRNE("hello", "hello2");
     // Expect equality.
     EXPECT_EQ(7 * 6, 42);
 }
