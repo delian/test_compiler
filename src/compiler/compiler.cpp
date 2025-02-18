271ea070e02cbc172e8fc86f8f057e59bfa3bae5
Delian Delchev <delian.delchev@gmail.com>
2025-02-18 17:15:19 +0000

    initial compiler
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -1,5 +1,12 @@
 #include "compiler.h"
 
+using namespace std;
+
+Compiler::Compiler(string module_name) : Module(std::make_unique<llvm::Module>(module_name, Context)), Builder(Context)
+{
+    this->Name = module_name; // TODO: check later if I need to keep it at all
+}
+
 void Compiler::compile(AST *node)
 {
     if (node == NULL)
@@ -9,10 +16,5 @@ void Compiler::compile(AST *node)
 
     switch (node->type)
     {
-        default:
-            printf("Unknown node type\n");
-            break;
     }
 }
-
-
--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -1,13 +1,28 @@
 #ifndef __COMPILER_H__
 #define __COMPILER_H__
 
-#include <stdio.h>
 #include "ast.h"
+#include <string>
+
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Verifier.h>
+
+using namespace std;
 
 class Compiler
 {
-    public:
-        void compile(AST *node);
+private:
+    string Name;
+
+    llvm::LLVMContext Context;
+    std::unique_ptr<llvm::Module> Module;
+    llvm::IRBuilder<> Builder;
+
+public:
+    Compiler(string module_name);
+    void compile(AST *node);
 };
 
 #endif
