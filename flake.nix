5a8a74c3dd4d98249a0e91d8f11c03719c1bedf3
Delian Delchev <delian.delchev@gmail.com>
2025-02-18 13:53:00 +0000

    nix
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,49 +4,22 @@
   description = "TestCompiler Nix Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixurl = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, ... }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      ply = (with pkgs; stdenv.mkDerivation {
-          pname = "ply";
-          version = "0.0.1";
-          src = ./.;
-          nativeBuildInputs = [
-            clang
-            cmake
-            ninja
-            conan
-            gcc
-            gcovr
-            gdb
-          ];
-          buildPhase = ''
-            conan profile detect
-            cmake -S . -B build -G Ninja
-            cmake --build build
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-          '';
-        }
-      );
-    in rec {
-      apps.${system}.default = flake-utils.lib.mkApp {
-        drv = packages.${system}.default;
-      };
-      packages.default = ply;
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          ply
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "testcompiler";
+        nativeBuildInputs = with pkgs; [
+          clang
+          cmake
+          git
+          ninja
         ];
       };
-    }
-  );
-
+    };
 }
