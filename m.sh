0c49778f89f91f7e39a3dff6f5f4db867c088a0b
Delian Delchev <delian.delchev@gmail.com>
2025-02-19 16:30:05 +0000

    fix
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- m.sh
+++ m.sh
@@ -1,4 +1,5 @@
 conan remote update conancenter --url https://center2.conan.io
 conan profile detect
+rm -rf ./build
 cmake -S . -B build -G Ninja
 cmake --build build -v --clean-first -j 8
--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 Compiler::Compiler(string module_name) : Module(std::make_unique<llvm::Module>(module_name, Context)), Builder(Context)
 {
-    this->Name = module_name; // TODO: check later if I need to keep it at all
+    this->Name = module_name; // TODO: check later if I want to keep it at all
 }
 
 void Compiler::compile(AST *node)
