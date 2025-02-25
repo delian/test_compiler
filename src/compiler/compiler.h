b77a6f1137ed3a3dd6fcb9fca65b2bc856ba8a76
Delian Delchev <delian.delchev@gmail.com>
2025-02-25 16:30:00 +0000

    some experiments
    Signed-off-by: Delian Delchev <delian.delchev@gmail.com>

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -4,25 +4,24 @@
 #include "ast.h"
 #include <string>
 
+#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
-#include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Verifier.h>
 
 using namespace std;
 
-class Compiler
-{
+class Compiler {
 private:
-    string Name;
+  string Name;
 
-    llvm::LLVMContext Context;
-    std::unique_ptr<llvm::Module> Module;
-    llvm::IRBuilder<> Builder;
+  llvm::LLVMContext Context;
+  std::unique_ptr<llvm::Module> Module;
+  llvm::IRBuilder<> Builder;
 
 public:
-    Compiler(string module_name);
-    void compile(AST *node);
+  Compiler(string module_name);
+  void compile(AST *node);
 };
 
 #endif
